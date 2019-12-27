31e693851f4623ea08c7aa9a17d8b614c0cc3225
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-27 13:26:01 +0300

    ivan

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 document.getElementById('chack').onclick = function() {
     var login = document.getElementById('login');
     var password = document.getElementById('password');
+
+    if (login == 'ivan' && password == '334455') {
+        alert('Добро пожаловать!')
+    }
 }
